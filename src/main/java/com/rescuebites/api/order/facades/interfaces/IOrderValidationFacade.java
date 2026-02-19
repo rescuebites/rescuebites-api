c2ee88ec0dd60616c8beb9356f48cd1c4f04d908
mroja <mrojaslopez03@gmail.com>
2026-02-19 14:07:31 -0300

    feat: implementa el facade del pedido #35 #36 #37 #39

---
+++ src/main/java/com/rescuebites/api/order/facades/interfaces/IOrderValidationFacade.java
@@ -0,0 +1,23 @@
+package com.rescuebites.api.order.facades.interfaces;
+
+import com.rescuebites.api.cart.data.models.Cart;
+import com.rescuebites.api.order.data.models.Order;
+
+import java.util.UUID;
+
+public interface IOrderValidationFacade {
+
+    Cart findCartByClientId(UUID clientId);
+
+    Order findOrderByIdAndClientId(UUID orderId, UUID clientId);
+
+    Order findOrderByIdAndCommerceId(UUID orderId, UUID commerceId);
+
+    void validateCartNotEmpty(Cart cart);
+
+    void validateAllItemsFromSameCommerce(Cart cart, UUID commerceId);
+
+    void validateStockForAllItems(Cart cart);
+
+    String generateOrderNumber();
+}
