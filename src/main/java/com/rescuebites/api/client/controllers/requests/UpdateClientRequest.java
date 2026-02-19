3242baa8e3b73868d1e12961b22427140324e3ae
mroja <mrojaslopez03@gmail.com>
2026-02-19 14:21:23 -0300

    feat: agrega el celular a la request de actualización del cliente.

--- src/main/java/com/rescuebites/api/client/controllers/requests/UpdateClientRequest.java
+++ src/main/java/com/rescuebites/api/client/controllers/requests/UpdateClientRequest.java
@@ -2,6 +2,7 @@ package com.rescuebites.api.client.controllers.requests;
 
 import com.rescuebites.api.client.data.enums.PreferenceType;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,14 +17,24 @@ import java.util.List;
 public class UpdateClientRequest {
 
         private String firstName;
+
         private String lastName;
+
         private LocalDate birthDate;
+
         private String address;
+
+        @Pattern(
+                regexp = "^\\+54(9)?[0-9]{10}$",
+                message = "El número de celular debe tener el formato válido argentino, ej: +54911XXXXXXXX"
+        )
+        private String phone;
 
         @Email(message = "El email debe ser válido")
         private String email;
 
         private String password;
+
         private String confirmPassword;
 
         private List<PreferenceType> preferences = Collections.emptyList();
