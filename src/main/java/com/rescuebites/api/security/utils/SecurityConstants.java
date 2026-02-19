f201ab41445b51d9c16d458e82b8d93e4daea749
mroja <mrojaslopez03@gmail.com>
2026-02-19 18:36:16 -0300

    feat: agrega endpoint público para webhook de Mercado Pago

--- src/main/java/com/rescuebites/api/security/utils/SecurityConstants.java
+++ src/main/java/com/rescuebites/api/security/utils/SecurityConstants.java
@@ -13,5 +13,8 @@ public class SecurityConstants {
             "/api/users/*/verify-account",
             "/api/users/resend-verification-account",
             "/api/users/reset-password/**",
+
+            // Webhook de Mercado Pago (debe estar público para recibir notificaciones)
+            "/api/v1/payments/webhook",
     };
 }
