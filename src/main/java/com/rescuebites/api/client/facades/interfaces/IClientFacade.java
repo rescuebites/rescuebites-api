a1870e36d75d1673ca2e7b9086fa8ab1fbe9efda
mroja <mrojaslopez03@gmail.com>
2026-02-19 13:47:13 -0300

    feat: agrega búsqueda de cliente por id.

--- src/main/java/com/rescuebites/api/client/facades/interfaces/IClientFacade.java
+++ src/main/java/com/rescuebites/api/client/facades/interfaces/IClientFacade.java
@@ -1,11 +1,16 @@
 package com.rescuebites.api.client.facades.interfaces;
 
 import com.rescuebites.api.client.controllers.requests.UpdateClientRequest;
+import com.rescuebites.api.client.data.models.Client;
 import com.rescuebites.api.shared.Image;
 import com.rescuebites.api.users.data.models.User;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 public interface IClientFacade {
+
+    Client findClientByIdOrThrowException(UUID clientId);
 
     Image processProfilePictureForCreation(MultipartFile profilePicture, String defaultProfilePictureUrl);
 
