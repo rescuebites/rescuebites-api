9b0080a9412c2d8da34881e49ac469c3d2771db1
mroja <mrojaslopez03@gmail.com>
2026-02-19 17:14:58 -0300

    perf: agrega cache a findByCommerceIdAndDeletedFalse y findByName.

--- src/main/java/com/rescuebites/api/commerce/repositories/ICommerceRepository.java
+++ src/main/java/com/rescuebites/api/commerce/repositories/ICommerceRepository.java
@@ -2,6 +2,7 @@ package com.rescuebites.api.commerce.repositories;
 
 import com.rescuebites.api.commerce.data.enums.CommerceTypeEnum;
 import com.rescuebites.api.commerce.data.models.Commerce;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @Repository
 public interface ICommerceRepository extends JpaRepository<Commerce, UUID> {
 
+    @Cacheable(value = "commerceById", key = "#commerceId")
     Optional<Commerce> findByCommerceIdAndDeletedFalse(UUID commerceId);
 
     boolean existsByNameAndDeletedFalse(String name);
