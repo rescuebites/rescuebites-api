ac0b33d3490f0c22c9cc3aeb5c240b96ddade230
mroja <mrojaslopez03@gmail.com>
2026-03-03 10:12:41 -0300

    fix: agrega constructor de Constants.

--- src/main/java/com/rescuebites/api/shared/utils/Constants.java
+++ src/main/java/com/rescuebites/api/shared/utils/Constants.java
@@ -4,6 +4,9 @@ import org.springframework.data.domain.PageRequest;
 
 public class Constants {
 
+    private Constants() {
+    }
+
     public static final PageRequest PRE_FILTER_PAGE_FOR_SEARCH = PageRequest.of(0, 100);
 
     public static final PageRequest PRE_FILTER_PAGE_FOR_SUGGESTIONS = PageRequest.of(0, 5);
