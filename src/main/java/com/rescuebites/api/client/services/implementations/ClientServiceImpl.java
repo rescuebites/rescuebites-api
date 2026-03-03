1534947bedd15755e33b16871279d8648728f99b
mroja <mrojaslopez03@gmail.com>
2026-03-03 09:30:58 -0300

    feat: elimina importe no utilizado.

--- src/main/java/com/rescuebites/api/client/services/implementations/ClientServiceImpl.java
+++ src/main/java/com/rescuebites/api/client/services/implementations/ClientServiceImpl.java
@@ -19,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
