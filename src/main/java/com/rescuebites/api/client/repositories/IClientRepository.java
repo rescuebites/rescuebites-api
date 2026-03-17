f1c1edf8eadae47b8a8eb11618583843b9b6d15c
mroja <mrojaslopez03@gmail.com>
2026-03-17 17:43:09 -0300

    feat: añade método para buscar cliente por userId en IClientRepository

--- src/main/java/com/rescuebites/api/client/repositories/IClientRepository.java
+++ src/main/java/com/rescuebites/api/client/repositories/IClientRepository.java
@@ -3,6 +3,9 @@ package com.rescuebites.api.client.repositories;
 import com.rescuebites.api.client.data.models.Client;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +13,7 @@ public interface IClientRepository extends JpaRepository<Client, UUID> {
 
     @EntityGraph(attributePaths = {"preferences", "user"})
     Optional<Client> findByClientIdAndDeletedFalse(UUID clientId);
+
+    @Query("SELECT c FROM clients c WHERE c.user.userId = :userId AND c.deleted = false")
+    Optional<Client> findByUserId(@Param("userId") UUID userId);
 }
