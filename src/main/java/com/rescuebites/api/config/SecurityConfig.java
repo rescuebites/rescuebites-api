fcaf81668fead3d06099be275090bfe4c2267616
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2026-05-05 18:07:37 -0300

    corsconfig class

--- src/main/java/com/rescuebites/api/config/SecurityConfig.java
+++ src/main/java/com/rescuebites/api/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.rescuebites.api.config;
 
-import java.util.List;
-
 import com.rescuebites.api.security.entrypoint.GlobalAuthenticationEntryPoint;
 import com.rescuebites.api.security.filter.JwtAuthenticationFilter;
 
@@ -19,9 +17,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -148,27 +143,3 @@ public class SecurityConfig {
         }
 }
 
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of(
-                "https://rescuebites-front-end.onrender.com"
-        ));
-
-        configuration.setAllowedMethods(List.of(
-                "GET", "POST", "PUT", "DELETE", "OPTIONS"
-        ));
-
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(false); // importante para JWT
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-}
