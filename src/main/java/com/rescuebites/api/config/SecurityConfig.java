3b7e8a852a2016da4eca0072023cde2db1841bde
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2026-05-05 17:57:45 -0300

    cors policy

--- src/main/java/com/rescuebites/api/config/SecurityConfig.java
+++ src/main/java/com/rescuebites/api/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.rescuebites.api.config;
 
+import java.util.List;
+
 import com.rescuebites.api.security.entrypoint.GlobalAuthenticationEntryPoint;
 import com.rescuebites.api.security.filter.JwtAuthenticationFilter;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,6 +19,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -139,4 +146,29 @@ public class SecurityConfig {
 
                 return http.build();
         }
+}
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of(
+                "https://rescuebites-front-end.onrender.com"
+        ));
+
+        configuration.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(false); // importante para JWT
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
